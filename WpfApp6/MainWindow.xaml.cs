7791a7d3e8a608d1238508a28ad997102eea6d40
maciej022 <maciejpieper56@gmail.com>
2024-10-30 17:39:56 +0100

    Dodanie  funkcjonalnosci przyciku reset (czyszczenie pol tekstowych) oraz oblicz przycisk

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -11,14 +11,47 @@ using System.Windows.Shapes;
 
 namespace WpfApp6
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window //klasa publiczna mainwindow oznacza glowne okno wpf ktore dziedziczy klase window
     {
-        public MainWindow()
+        // Obsługa kliknięcia przycisku "Oblicz liczbe wlosow"
+        private void Oblicz_Przycisk(object sender, RoutedEventArgs e) //private obliczanie przycisku
         {
-            InitializeComponent();
+            try // zawiera kod ktory generuje bledy
+            {
+
+                //uzylem zmiennej typu float poniewaz jest to typ zmiennoprzecinkowy 
+                // Odczytanie i parsowanie danych wejściowych jako float
+                float obwodGlowy = float.Parse(txtObwod.Text);
+                float wysokoscCzola = float.Parse(txtWysokoscCzola.Text);
+                float gestoscWlosow = float.Parse(txtGestosc.Text);
+
+                // Obliczenie liczby włosow+
+
+
+
+            }
+            catch (FormatException) //catch reaguje gdy uzytkownik nie wprowadzi zadnych danych lub wprowadzi inne dane
+            {
+                MessageBox.Show("Wprowadź poprawne wartości liczbowe."); //messagebox to wyskakujacy komunikat ktory informuje uzytkownika o zle wprowadzonych danych
+            }
+        }
+
+
+       
+        // Obsluga klikniecia przycisku "Reset"
+        private void Reset_Przycisk()
+        {
+            // Czyszczenie pol tekstowych
+            txtObwod.Clear();
+            txtWysokoscCzola.Clear();
+            txtGestosc.Clear();
+            txtWynik.Text = string.Empty;
         }
     }
 }
+
+
+
+
+
+   
