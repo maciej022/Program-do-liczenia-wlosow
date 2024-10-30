ff2f12de32ce8d92e2a4a83bb32a2f5116e5c8bd
maciej022 <maciejpieper56@gmail.com>
2024-10-30 18:03:18 +0100

    Dodanie klasy glowa poprawienie bledow w poprzednim kodzie

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -1,57 +1,75 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp6
 {
-    public partial class MainWindow : Window //klasa publiczna mainwindow oznacza glowne okno wpf ktore dziedziczy klase window
+    public partial class MainWindow : Window // Klasa publiczna MainWindow oznacza główne okno WPF, które dziedziczy klasę Window
     {
-        // Obsługa kliknięcia przycisku "Oblicz liczbe wlosow"
-        private void Oblicz_Przycisk(object sender, RoutedEventArgs e) //private obliczanie przycisku
+        public MainWindow()
         {
-            try // zawiera kod ktory generuje bledy
-            {
+            InitializeComponent(); // Inicjalizuje komponenty okna
+        }
 
-                //uzylem zmiennej typu float poniewaz jest to typ zmiennoprzecinkowy 
+        // Obsługa kliknięcia przycisku "Oblicz liczbę włosów"
+        private void Oblicz_Click(object sender, RoutedEventArgs e) // Zmieniono metodę na przyjmującą parametry
+        {
+            try // Zawiera kod, który generuje błędy
+            {
+                // Użyłem zmiennej typu float, ponieważ jest to typ zmiennoprzecinkowy 
                 // Odczytanie i parsowanie danych wejściowych jako float
                 float obwodGlowy = float.Parse(txtObwod.Text);
                 float wysokoscCzola = float.Parse(txtWysokoscCzola.Text);
                 float gestoscWlosow = float.Parse(txtGestosc.Text);
 
-                // Obliczenie liczby włosow+
+                // Tworzenie instancji klasy Glowa
+                Glowa glowa = new Glowa();
+                glowa.ObliczLiczbeWlosow(gestoscWlosow, obwodGlowy, wysokoscCzola); // Obliczenie liczby włosów
 
-
-
+                // Wyświetlenie wyniku
+                txtWynik.Text = $"Szacunkowa liczba włosów: {glowa.LiczbaWlosow:F}\n" +
+                                $"Procent względem przeciętnej: {glowa.PorownajDoPrzecietnej():F2}%";
             }
-            catch (FormatException) //catch reaguje gdy uzytkownik nie wprowadzi zadnych danych lub wprowadzi inne dane
+            catch (FormatException) // Catch reaguje, gdy użytkownik nie wprowadzi danych lub wprowadzi inne dane
             {
-                MessageBox.Show("Wprowadź poprawne wartości liczbowe."); //messagebox to wyskakujacy komunikat ktory informuje uzytkownika o zle wprowadzonych danych
+                MessageBox.Show("Wprowadź poprawne wartości liczbowe."); // MessageBox to wyskakujący komunikat informujący użytkownika o złych wprowadzonych danych
             }
         }
 
-
-       
-        // Obsluga klikniecia przycisku "Reset"
-        private void Reset_Przycisk()
+        // Obsługa kliknięcia przycisku "Reset"
+        private void Reset_Click(object sender, RoutedEventArgs e) // Zmieniono metodę na przyjmującą parametry
         {
-            // Czyszczenie pol tekstowych
+            // Czyszczenie pól tekstowych
             txtObwod.Clear();
             txtWysokoscCzola.Clear();
             txtGestosc.Clear();
-            txtWynik.Text = string.Empty;
+            txtWynik.Text = string.Empty; // Resetowanie wyniku
+        }
+    }
+
+    public class Glowa
+    {
+        private const float PrzecietnaLiczbaWlosow = 100000f; // Używamy 'f' dla float
+        private float liczbaWlosow; // Typ float dla liczby włosów
+
+        public float LiczbaWlosow => liczbaWlosow; // Właściwość do uzyskania liczby włosów
+
+        // Oblicza powierzchnię głowy na podstawie obwodu i wysokości czoła
+        private float ObliczPowierzchnie(float obwod, float wysokosc)
+        {
+            return (obwod * wysokosc) / (4 * (float)Math.PI); // Używamy Math.PI dla operacji na float ponieważ głowa nie jest kwadratem
+        }
+
+        // Oblicza liczbę włosów na podstawie gęstości, obwodu i wysokości czoła
+        public void ObliczLiczbeWlosow(float gestosc, float obwod, float wysokosc)
+        {
+            float powierzchnia = ObliczPowierzchnie(obwod, wysokosc);
+            liczbaWlosow = gestosc * powierzchnia; // Wyliczenie liczby włosów
+        }
+
+        // Oblicza procentową różnicę w stosunku do przeciętnej liczby włosów
+        public float PorownajDoPrzecietnej()
+        {
+            return ((liczbaWlosow - PrzecietnaLiczbaWlosow) * 100) / PrzecietnaLiczbaWlosow; // Obliczenie różnicy procentowej
         }
     }
 }
-
-
-
-
-
-   
